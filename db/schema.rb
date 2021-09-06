f4155de2af2814e71b5eeb84ac7942487fd7e4fd
Rosie Choi <65037553+Unevenmango35@users.noreply.github.com>
2021-09-06 16:42:35 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,13 @@
 
 ActiveRecord::Schema.define(version: 20180314210852) do
 
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+  end
+
+  def down
+    drop_table :users
+  end
+
 end
