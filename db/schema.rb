d0bde5bcc96764a9028299ea6c7bb9f20d67398d
Rosie Choi <65037553+Unevenmango35@users.noreply.github.com>
2021-09-09 15:31:14 -0400

    commit tree 7c8326ba5b90aebb13441db8a89a1d878a563489

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180314210852) do
 
-  create_table "users", force: :cascade do |t|
+  create_table "user", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
-  end
-
-  def down
-    drop_table :users
   end
 
 end
